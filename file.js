e698e76afe91f4b7879a41c8fb815f64710e9956
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-07-10 18:54:53 +0545

    fs

--- file.js
+++ file.js
@@ -1,6 +1,56 @@
 const fs = require("fs");
 
-fs.readFile("./h.txt", (err, data) => {
+// fs.readFile("./files/h.txt", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+//   console.log(data.toString());
+// });
+
+// fs.readFile("./files/h.txt", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+console.log("i am 1st haha");
+
+//instead of hardcoding file location
+
+const path = require("path");
+
+console.log(__dirname);
+fs.readFile(path.join(__dirname, "files", "h.txt"), "utf-8", (err, data) => {
   if (err) throw err;
   console.log(data);
+});
+
+fs.writeFile(
+  path.join(__dirname, "files", "write.txt"),
+  "created new file",
+  (err) => {
+    if (err) throw err;
+    console.log("Write op");
+
+    fs.appendFile(
+      path.join(__dirname, "files", "write.txt"),
+      "\n\n appending",
+      (err) => {
+        if (err) throw err;
+        console.log("Append op");
+      }
+    );
+
+    fs.rename(
+      path.join(__dirname, "files", "write.txt"),
+      "rename.txt",
+      (err) => {
+        if (err) throw err;
+        console.log("Rename op");
+      }
+    );
+  }
+);
+
+process.on("uncaughtException", (err) => {
+  console.error(`Uncaught Error ${err}`);
+  process.exit(1);
 });
