8f16117a9e4121880c9fff26f2adb7399b698905
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-07-10 18:33:26 +0545

    export

--- os&path.js
+++ os&path.js
@@ -1,19 +1,24 @@
-console.log(global);
+// console.log(global);
 
-const os = require("os");
+// const os = require("os");
 
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
 
-const path = require("path");
+// const path = require("path");
 
-console.log("path...");
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
+// console.log("path...");
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
 
-console.log(path.parse(__filename));
+// console.log(path.parse(__filename));
+
+const { add, sub, mul } = require("./math");
+
+console.log(add(3, 3));
+console.log(mul(2, 3));
