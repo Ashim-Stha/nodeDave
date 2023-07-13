2809908e5af108dcb0e27cb6440a7f70dd7f4b03
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-07-13 18:40:30 +0545

    routeHandlers

--- express.js
+++ express.js
@@ -16,6 +16,36 @@ app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html"); //302 by default but we need 301
 });
 
+//route handlers
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to open hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World!");
+  }
+);
+
+//chaining route handlers
+const one = (req, res, next) => {
+  console.log("One");
+  next();
+};
+
+const two = (req, res, next) => {
+  console.log("Two");
+  next();
+};
+
+const three = (req, res) => {
+  console.log("three");
+  res.send("Chaining route hadlers");
+};
+
+app.get("/chain(.html)?", [one, two, three]);
+
 app.get("/*", (req, res) => {
   //we have 404.html so it sends status code 200
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
