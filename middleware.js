4ffb146b56f9f58fdf557081ab2ac987c71542a1
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-07-13 22:50:57 +0545

    cors-Final

--- middleware.js
+++ middleware.js
@@ -21,7 +21,22 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(logger);
 
 //cors=cross origin resource sharing
-app.use(cors());
+const allowedSite = [
+  "https://www.google.com",
+  "http://localhost:3500",
+  "https://127.0.0.1:5500",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedSite.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by cors"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
@@ -35,9 +50,28 @@ app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html"); //302 by default but we need 301
 });
 
-app.get("/*", (req, res) => {
-  //we have 404.html so it sends status code 200
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+// app.get("/*", (req, res) => {
+//   //we have 404.html so it sends status code 200
+//   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+// });
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({
+      error: "404 Not Found",
+    });
+  } else {
+    res.type("txt".send("404 Not Found"));
+  }
+});
+
+//error handler for cors
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).send(err.message);
 });
 
 app.listen(port, () => {
